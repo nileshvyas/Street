6a033c00ab418578a10525856f54a711029c5e07
nileshvyas <54304642+nileshvyas@users.noreply.github.com>
2019-08-20 15:33:10 +0530

    Update Street.py

--- Street.py
+++ Street.py
@@ -1,10 +1,22 @@
-# Define a function that will post on server every 15 Seconds  
-  
+# Define a function that will post on server every 15 Seconds 
+
+import requests
+import urllib.request 
+
 def read_data_thingspeak():
   print("hello")
+  b=5
+  URl='https://api.thingspeak.com/update?api_key='
+  KEY='B9RDBVP7MF9JYNEV'
+  HEADER='&field1={}&field2={}'.format(b,b)
+  NEW_URL=URl+KEY+HEADER
+  print(NEW_URL)
+  data=urllib.request.urlopen(NEW_URL)
+  print(data)
  
 
 if __name__ == '__main__':
-    
-  read_data_thingspeak()
-  #hingspeak_post()
+  
+  while(1):
+    read_data_thingspeak()
+    #thingspeak_post()
