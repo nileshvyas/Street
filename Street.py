9e9b741581a61afbf0e32895021c64ae36b51fe8
nileshvyas <54304642+nileshvyas@users.noreply.github.com>
2019-08-20 16:37:09 +0530

    Update Street.py

--- Street.py
+++ Street.py
@@ -1,13 +1,42 @@
-# Define a function that will post on server every 15 Seconds 
-
+import urllib.request
 import requests
-import urllib.request 
+import threading
+import json
 
+
+
+
+
+# Define a function that will post on server every 15 Seconds  
+  
 def read_data_thingspeak():
-  print("hello")
-  b=5
+  threading.Timer(15,read_data_thingspeak).start()
+  URL=' https://api.thingspeak.com/channels/538984/feeds.json?api_key='
+  KEY='X34X42TNNLBL2J21'
+  HEADER='&results=10'
+  NEW_URL=URL+KEY+HEADER
+  print(NEW_URL)
+
+  get_data=requests.get(NEW_URL).json()
+  print(get_data)
+  channel_id=get_data['channel']['id']
+
+  feild_1=get_data['feeds']
+  print("field_1 :")
+  print(feild_1)
+
+  t=[]
+  for x in feild_1:
+    #print(x['field1'])
+    t.append(x['field1'])
+    a=x['field1']
+    print('a=',a)
+
+  #def thingspeak_post(self):
+  b=max(t,key=t.count)
+  print("maximum data ",max(t,key=t.count)) 
   URl='https://api.thingspeak.com/update?api_key='
-  KEY='9DL7U7WNTMGJGIQM'
+  KEY='B9RDBVP7MF9JYNEV'
   HEADER='&field1={}&field2={}'.format(b,b)
   NEW_URL=URl+KEY+HEADER
   print(NEW_URL)
@@ -16,7 +45,6 @@ def read_data_thingspeak():
  
 
 if __name__ == '__main__':
-  
-  while(1):
-    read_data_thingspeak()
-    #thingspeak_post()
+    
+  read_data_thingspeak()
+  #hingspeak_post()
