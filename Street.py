ec62285ce7353c4fe2321784df54567a487b7081
nileshvyas <54304642+nileshvyas@users.noreply.github.com>
2019-09-04 17:03:56 +0530

    Update Street.py

--- Street.py
+++ Street.py
@@ -2,25 +2,36 @@ import urllib.request
 import requests
 import threading
 import json
-from flask import Flask
+import time
 
-app=Flask(__name__)
-@app.route("/")
-def hello():
-    return "hello world on root"
+import random
 
 
-@app.route("/harry")
-def hello1():
-    return "hello world in harry"
+import datetime  
+from datetime import timedelta
+
+utc=datetime.datetime.now(datetime.timezone.utc).strftime("%Y-%m-%dT%H:%M:%S")
+print(utc)
+
+
+
+#import datetime 
+basedate=str(utc)
+formatfrom="%Y-%m-%dT%H:%M:%S"
+formatto="%H:%M:%S"
+from datetime import datetime
+current_time=datetime.strptime(basedate,formatfrom).strftime(formatto)
+
+
+
 
 # Define a function that will post on server every 15 Seconds  
   
 def read_data_thingspeak():
-  threading.Timer(15,read_data_thingspeak).start()
+  threading.Timer(60,read_data_thingspeak).start()
   URL=' https://api.thingspeak.com/channels/538984/feeds.json?api_key='
   KEY='X34X42TNNLBL2J21'
-  HEADER='&results=10'
+  HEADER='&results=7'
   NEW_URL=URL+KEY+HEADER
   print(NEW_URL)
 
@@ -31,28 +42,74 @@ def read_data_thingspeak():
   feild_1=get_data['feeds']
   print("field_1 :")
   print(feild_1)
+ 
 
   t=[]
+  entry_id=[]
+  time=[]
   for x in feild_1:
     #print(x['field1'])
     t.append(x['field1'])
     a=x['field1']
     print('a=',a)
+    entry_id.append(x['entry_id'])
+    time.append(x['created_at'])
 
-  #def thingspeak_post(self):
-  b=max(t,key=t.count)
-  print("maximum data ",max(t,key=t.count)) 
-  URl='https://api.thingspeak.com/update?api_key='
-  KEY='B9RDBVP7MF9JYNEV'
-  HEADER='&field1={}&field2={}'.format(b,b)
-  NEW_URL=URl+KEY+HEADER
-  print(NEW_URL)
-  data=urllib.request.urlopen(NEW_URL)
-  print(data)
+  print("Entry Id: ")
+  print(entry_id[6])
+  print(time[6])
+  basedate=time[6]
+  print("BASE_TIME: ",type(basedate))
+  formatfrom="%Y-%m-%dT%H:%M:%Sz"
+  formatto="%H:%M:%S"
+  last_time=datetime.strptime(basedate,formatfrom).strftime(formatto)
+  print("Current time: ",current_time)
+  print("Last Time: ",last_time)
+  FMT = '%H:%M:%S'
+  tdelta = datetime.strptime(current_time,FMT)- datetime.strptime(last_time,FMT)
+  print("tdelta")
+  print(type(str(tdelta)))
+  print(type(last_time))
+  #=datetime.datetime.strptime(last_time,"%H")
+  #rint("b: ",b)
+  list1=[]
+  def Convert(string):
+    li = list(string.split(":")) 
+    return li
+  list1=(Convert(str(tdelta)))
+  print(list1)
+  print(type(list1[0]))
+  li=[]
+  li=list(list1[0].split(" "))
+  print(li)
+  list1.insert(0,li)
+  del list1[1]
+  print("LIST1: ",list1[0][0])
+  if ('day' in list1[0]):
+     print("Day Present")
+  else:
+    list1[0].insert(0,'0')
+    list1[0].insert(1,'day,')
+    print("Update list:",list1)
+    
+  print("list1",list1)  
+  total_minute=(int(list1[0][0])*(-1)*24+int(list1[0][2]))*60+int(list1[1])
+  print("Total Minute: ",total_minute)
  
+  
+  if(total_minute<=30):
+    b=max(t,key=t.count)
+    print("maximum data ",max(t,key=t.count)) 
+    URl='https://api.thingspeak.com/update?api_key='
+    KEY='B9RDBVP7MF9JYNEV'
+    HEADER='&field1={}&field2={}'.format(b,b)
+    NEW_URL=URl+KEY+HEADER
+    print(NEW_URL)
+    data=urllib.request.urlopen(NEW_URL)
+    print(data)
+
 
 if __name__ == '__main__':
     
   read_data_thingspeak()
-  app.run(host='0.0.0.0',port=5000,debug=True)
   #hingspeak_post()
